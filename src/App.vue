28301366e0e1899edc4fd3b6cc8dfeab269631e7
ljlCoud <1339203194@qq.com>
2024-02-14 14:24:07 +0800

    增加ArcGISJSAPI实例化地图

--- src/App.vue
+++ src/App.vue
@@ -3,19 +3,24 @@
     <el-container class="app-out-pannel">
       <el-header class="sys-header">一张图项目系统</el-header>
       <el-container class="app-content-pannel">
-        <el-aside width="200px" class="sys-meau">左侧菜单</el-aside>
-        <el-main>地图区域</el-main>
+        <el-aside width="200px" class="sys-menu">左侧菜单</el-aside>
+        <el-main class="sys-content">
+          <Mapview />
+        </el-main>
       </el-container>
     </el-container>
   </div>
 </template>
 
 <script>
+  import Mapview from './components/common/Mapview';
 
   export default {
     name: 'App',
-    components: {}
-  }
+    components: {
+      Mapview,
+    },
+  };
 </script>
 
 <style>
@@ -40,8 +45,12 @@
     font-size: 20px;
   }
 
-  .sys-meau {
-    background-color: #909399;
+  .sys-menu {
+    background-color: #c0c1c2;
 
+  }
+
+  .sys-content {
+    padding: 5px;
   }
 </style>
